dd0d0c4e78b8e58c312b20297c65ee1732567bbf
Addy <123188947+addymwenda12@users.noreply.github.com>
2023-09-14 12:46:25 +0300

    Class definition with specified attributes

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -6,11 +6,11 @@ class Rectangle:
 		self.__width = width
 		self.__height = height
 
-	# Property
+	@property
 	def width(self):
 		return self.__width
 	
-	# Width.setter
+	@width.setter
 	def width(self, value):
 		if not isinstance(value, int):
 			raise TypeError("width must be an integer")
@@ -18,11 +18,11 @@ class Rectangle:
 			raise ValueError("width must be >= 0")
 		self.__width = value
 
-	# Property
+	@property
 	def height(self):
 		return self.__height
 	
-	# Height setter
+	@height.setter
 	def height(self, value):
 		if not isinstance(value, int):
 			raise TypeError("height must be an integer")
