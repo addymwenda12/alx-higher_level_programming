a6518ae70abf87a7c9f32ee6b1b63a6d99353dda
Addy <123188947+addymwenda12@users.noreply.github.com>
2023-09-17 17:42:59 +0300

    Prints lists in ascending order

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-
+"""Inherits from list"""
 class MyList(list):
+	"""Prints lists in ascending order"""
 	def print_sorted(self):
 		sorted_list = sorted(self)
 		print(sorted_list)
