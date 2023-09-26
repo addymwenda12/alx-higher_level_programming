d55f6621947e637a3c7a6fa314bc8ed6ed238f37
Addy <123188947+addymwenda12@users.noreply.github.com>
2023-09-26 12:44:11 +0300

    Module subclass rectangle

--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -1,20 +1,20 @@
 #!/usr/bin/python3
-"""class Rectangle that inherits from BaseGeometry"""
+""" module subclass rectangle
+"""
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
 
 class Rectangle(BaseGeometry):
-    """Inheritation from BaseGeometry"""
+    """Rectangle that inherits from BaseGeometry"""
     def __init__(self, width, height):
-        self.__width = 0  # Initialize width to 0
-        self.__height = 0  # Initialize height to 0
-        self.integer_validator("width", width)  # Validate and set width
-        self.integer_validator("height", height)  # Validate and set height
-        self.__width = width  # Set the width
-        self.__height = height  # Set the height
+        BaseGeometry.integer_validator(self, 'height', height)
+        self.__height = height
+        BaseGeometry.integer_validator(self, 'width', width)
+        self.__width = width
 
     def area(self):
-        """Compute the area of the rectangle."""
         return self.__width * self.__height
 
     def __str__(self):
-        """Return the string representation of the rectangle."""
+
         return "[Rectangle] {}/{}".format(self.__width, self.__height)
